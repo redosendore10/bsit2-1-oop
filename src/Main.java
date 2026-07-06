1728cce50d2a73a9059be615f3f350b0793e2396
Redosendo Ralph <redosendore10595@liceo.edu.ph>
2026-07-06 13:26:42 +0800

    lab activity 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Student Information System");
+            System.out.println("1. add student");
+            System.out.println("2. view student");
+            System.out.println("3. search");
+            System.out.println("4. view statistics");
+            System.out.println("5. exit terminal");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                case 5: running = false; break;
+                default:
+            }
+        }
     }
 }
